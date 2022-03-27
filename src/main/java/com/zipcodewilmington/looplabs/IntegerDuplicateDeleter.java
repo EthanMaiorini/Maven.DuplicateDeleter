57841bbcbb9f3a4bb7590a4c4d4f9ae2b6640b88
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-27 10:22:55 -0400

    Just Starting

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,73 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public Integer[] intArray;
+
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+        this.intArray = Arrays.copyOf(intArray,intArray.length);
+    }
+
+    @Override
+    public  Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        //Integer[] newArray = new Integer[intArray.length];
+        for(int x =0;x<intArray.length;x++){
+            Integer occ = getNumberOfOccurrences(intArray,intArray[x]);
+            if(occ >= maxNumberOfDuplications){
+                intArray = removeValue(intArray,intArray[x]);
+                x--;
+            }
+        }
+        final Integer[] intArr2 = Arrays.copyOf(intArray,intArray.length);
+        return intArr2;
+    }
+
+    @Override
+    public final Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        for(int x =0;x<intArray.length;x++){
+            Integer occ = getNumberOfOccurrences(intArray,intArray[x]);
+            if(occ == exactNumberOfDuplications){
+                super.array = removeValue(intArray,intArray[x]);
+                x--;
+            }
+
+        }
+        //super.array = Arrays.copyOf(intArray,intArray.length);
+        final Integer[] intArr2 =  Arrays.copyOf(intArray,intArray.length);
+        return intArr2;
+    }
+
+    public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
+        int count = 0;
+        for(Object s:objectArray){
+            if(s.equals(objectToCount)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static Integer[] removeValue(Object[] objectArray, Object objectToRemove) {
+
+        int count = getNumberOfOccurrences(objectArray, objectToRemove);
+        int newArrayLength = objectArray.length - count;
+        Object[] newArray = new Object[newArrayLength];
+        int x = 0;
+        for (int i = 0; i < objectArray.length; i++) {
+            if (!(objectArray[i].equals(objectToRemove))) {
+                newArray[x] = objectArray[i];
+                x++;
+            }
+        }
+        Integer[] integerArray = Arrays.copyOf(newArray, newArray.length, Integer[].class);
+        return integerArray;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,69 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public String[] intArray;
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+        this.intArray = Arrays.copyOf(intArray,intArray.length);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        for(int x =0;x<intArray.length;x++){
+            Integer occ = getNumberOfOccurrences(intArray,intArray[x]);
+            if(occ >= maxNumberOfDuplications){
+                intArray = removeValue(intArray,intArray[x]);
+                x--;
+            }
+        }
+        super.array = Arrays.copyOf(intArray,intArray.length);
+        return super.array;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        for(int x =0;x<intArray.length;x++){
+            Integer occ = getNumberOfOccurrences(intArray,intArray[x]);
+            if(occ == exactNumberOfDuplications){
+                super.array = removeValue(intArray,intArray[x]);
+                x--;
+            }
+
+        }
+        super.array = Arrays.copyOf(intArray,intArray.length);
+        return super.array;
+    }
+
+    public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
+        int count = 0;
+        for(Object s:objectArray){
+            if(s.equals(objectToCount)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String[] removeValue(Object[] objectArray, Object objectToRemove) {
+
+        int count = getNumberOfOccurrences(objectArray, objectToRemove);
+        int newArrayLength = objectArray.length - count;
+        Object[] newArray = new Object[newArrayLength];
+        int x = 0;
+        for (int i = 0; i < objectArray.length; i++) {
+            if (!(objectArray[i].equals(objectToRemove))) {
+                newArray[x] = objectArray[i];
+                x++;
+            }
+        }
+        String[] integerArray = Arrays.copyOf(newArray, newArray.length, String[].class);
+        return integerArray;
+    }
+
 }
